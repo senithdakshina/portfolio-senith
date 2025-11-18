6652448fbbe456ae3994537d3e55ef73ba66b354
senith <senithdakshina@gmail.com>
2025-11-18 16:42:24 +0530

    star and meteors appear ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { ThemeToggle } from '../components/ThemeToggle';
+import { StarBackground } from '../components/StarBackground';
 
 export const Home =() =>{
     return <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
        
         <ThemeToggle></ThemeToggle>
+        <StarBackground></StarBackground>
         </div>;
 };
